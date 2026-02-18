2686279386a5a5d229c0f1117dc90db29badc1cf
anandparul01 <londonparul@gmail.com>
2026-02-18 14:44:24 +0000

    made a small change in imports

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pathlib import Path
 from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.llms import HuggingFacePipeline
